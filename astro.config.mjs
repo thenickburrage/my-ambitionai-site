228c0906eb88804e6eaac1cee412d973c72e23b4
thenickburrage <nb@theambitionsagency.com>
2026-04-05 12:47:09 -0500

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,6 @@
 import cloudflare from "@astrojs/cloudflare";
 import react from "@astrojs/react";
+import tailwind from "@astrojs/tailwind";
 import { d1, r2, sandbox } from "@emdash-cms/cloudflare";
 import { formsPlugin } from "@emdash-cms/plugin-forms";
 import { webhookNotifierPlugin } from "@emdash-cms/plugin-webhook-notifier";
@@ -7,22 +8,25 @@ import { defineConfig } from "astro/config";
 import emdash from "emdash/astro";
 
 export default defineConfig({
-	output: "server",
-	adapter: cloudflare(),
-	image: {
-		layout: "constrained",
-		responsiveStyles: true,
-	},
-	integrations: [
-		react(),
-		emdash({
-			database: d1({ binding: "DB", session: "auto" }),
-			storage: r2({ binding: "MEDIA" }),
-			plugins: [formsPlugin()],
-			sandboxed: [webhookNotifierPlugin()],
-			sandboxRunner: sandbox(),
-			marketplace: "https://marketplace.emdashcms.com",
-		}),
-	],
-	devToolbar: { enabled: false },
+		output: "server",
+		adapter: cloudflare(),
+		image: {
+					layout: "constrained",
+					responsiveStyles: true,
+		},
+		integrations: [
+					react(),
+					tailwind({
+									applyBaseStyles: false,
+					}),
+					emdash({
+									database: d1({ binding: "DB", session: "auto" }),
+									storage: r2({ binding: "MEDIA" }),
+									plugins: [formsPlugin()],
+									sandboxed: [webhookNotifierPlugin()],
+									sandboxRunner: sandbox(),
+									marketplace: "https://marketplace.emdashcms.com",
+					}),
+				],
+		devToolbar: { enabled: false },
 });
